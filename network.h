24e55dfb7665925e07c5a1347f6321124f6e150d
Nicholas Capel <nrjc2@cam.ac.uk>
2016-05-21 22:12:44 +0100

    Added getnumberofinputs function to network class

--- network.h
+++ network.h
@@ -41,27 +41,30 @@ class network {
  public:
   devlink devicelist (void);
     /* Returns list of devices                                             */
- 
+
   devlink finddevice (name id);
    /* Returns link to device with specified name. Returns NULL if not       */
    /* found.                                                               */
- 
+
   inplink findinput (devlink dev, name id);
     /* Returns link to input of device pointed to by dev with specified    */
     /* name.  Returns NULL if not found.                                    */
- 
+
+  int getnumberofinputs (name id);
+  /* Given a device name, gets the number of inputs that the device can take. */
+
   outplink findoutput (devlink dev, name id);
     /* Returns link to output of device pointed to by dev with specified   */
     /* name.  Returns NULL if not found.                                    */
- 
+
   void adddevice (devicekind dkind, name did, devlink& dev);
     /* Adds a device to the device list with given name and returns a link */
     /* to it via 'dev'.                                                    */
- 
+
   void addinput (devlink dev, name iid);
     /* Adds an input to the device pointed to by 'dev' with the specified  */
     /* name.                                                               */
- 
+
   void addoutput (devlink dev, name oid);
     /* Adds an output to the device pointed to by 'dev' with the specified */
     /* name.                                                               */
@@ -70,10 +73,10 @@ class network {
     /* Makes a connection between the 'inp' input of device 'idev' and the */
     /* 'outp' output of device 'odev'. 'ok' is set true if operation       */
     /* succeeds.                                                           */
- 
+
   void checknetwork (bool& ok);
     /* Checks that all inputs are connected to an output.                  */
- 
+
   network (names* names_mod);
   /* Called on system initialisation.                                      */
 
